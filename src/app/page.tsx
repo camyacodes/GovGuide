c003a1e205ed801db635c735552eb97c150613d0
camyacodes <camyacodes@gmail.com>
2024-04-17 13:22:17 -0400

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,42 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
-  return <Button>Click me</Button>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+  return (
+    <div className="w-screen min-h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">
+              Chat with any Goverment Document
+            </h1>
+            <UserButton afterSignOutUrl="/"></UserButton>
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Start Chatting</Button>}
+          </div>
+          <p className="max-w-xl mt-2 text-lg text-slate-800">
+            Join our community and decode complex policies and legislation
+            effortlessly with AI assistance.
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>Upload File</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Get Started
+                  <LogIn className="w-4 h-4 ml-2"></LogIn>
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
