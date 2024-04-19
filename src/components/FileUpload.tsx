da9a000f90dbd66334d23e2e28571ed786a53876
camyacodes <camyacodes@gmail.com>
2024-04-18 21:20:44 -0400

    added aws s3 bucket

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { uploadtoS3 } from "@/lib/s3";
 import { log } from "console";
 import { Inbox } from "lucide-react";
 import React from "react";
@@ -8,8 +9,20 @@ const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("file size exceeds limit");
+        return;
+      }
+
+      try {
+        const data = await uploadtoS3(file);
+        console.log("data", data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
@@ -24,7 +37,8 @@ const FileUpload = () => {
         <input {...getInputProps()} />
         <>
           <Inbox className="w-10 h-10 text-slate-400"></Inbox>
-          <p className="text-slate-400">Upload File</p>
+          <p className="text-slate-400">Upload or Drop File</p>
+          <p className="text-xs text-slate-400">*Must be PDF and up to 10MB</p>
         </>
       </div>
     </div>
